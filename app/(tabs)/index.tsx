3d1945a2ab24769d319a94e5b422fff0260bbc96
Chin123-hash <chinchunwei571@gmail.com>
2025-12-15 09:28:35 +0800

    Test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginScreen from '../../src/screens/LoginScreen';
+import LoginScreen from '../../src/screens/CustomerMainPage';
 
 export default function Page() {
   return <LoginScreen />;
